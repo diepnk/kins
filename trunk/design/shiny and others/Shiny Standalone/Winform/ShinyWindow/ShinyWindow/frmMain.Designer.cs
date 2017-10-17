491bb6317d64472b2030449c4ec30361ba4e45b4
Diep Khac Nguyen <diepkn@gcs-vn.com>
2017-10-17 16:02:41 +0900

    add document and .Net code to package Shiny standalone

---
+++ trunk/design/shiny and others/Shiny Standalone/Winform/ShinyWindow/ShinyWindow/frmMain.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace ShinyWindow
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wbrsApp = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // wbrsApp
+            // 
+            this.wbrsApp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wbrsApp.Location = new System.Drawing.Point(0, 0);
+            this.wbrsApp.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbrsApp.Name = "wbrsApp";
+            this.wbrsApp.ScriptErrorsSuppressed = true;
+            this.wbrsApp.Size = new System.Drawing.Size(568, 313);
+            this.wbrsApp.TabIndex = 0;
+            this.wbrsApp.Url = new System.Uri("http://www.vnexpress.net", System.UriKind.Absolute);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 313);
+            this.Controls.Add(this.wbrsApp);
+            this.Name = "frmMain";
+            this.Text = "Shiny Demo";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser wbrsApp;
+    }
+}
+
---
+++ trunk/design/shiny and others/Shiny Standalone/Winform/StartShinyDemo/StartShinyDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace StartShinyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location));
+            Process scriptProc = new Process();
+            scriptProc.StartInfo.FileName = @"run.vbs";
+            scriptProc.StartInfo.WorkingDirectory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location); //<---very important 
+            scriptProc.StartInfo.Arguments = "//B //Nologo run.vbs";
+            scriptProc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden; //prevent console window from popping up
+            scriptProc.Start();
+            scriptProc.Close();
+        }
+    }
+}
